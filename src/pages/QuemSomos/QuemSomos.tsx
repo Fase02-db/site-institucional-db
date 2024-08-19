0f9cc13551b9945c53b9ceaf56fb581cd46b1700
pedro felipe <pedrofelipinho@gmail.com>
2024-08-19 11:50:06 -0300

    Criando as rotas e o teste das rotas

---
+++ src/pages/QuemSomos/QuemSomos.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const QuemSomos: React.FC = () => {
+  return (
+    <div>
+      <h1>Quem somos?</h1>
+      <Link to="/quem-somos"></Link>
+    </div>
+  );
+};
+
+export default QuemSomos;
