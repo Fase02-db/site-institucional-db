e7a2afb87a7a9baf0ac6f9e786e28df026342b9a
pedro felipe <pedrofelipinho@gmail.com>
2024-08-21 10:45:53 -0300

    Deixando fonte padrão

--- src/pages/Cases/Cases.tsx
+++ src/pages/Cases/Cases.tsx
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom';
 const Cases: React.FC = () => {
   return (
     <div>
-      <h1 style={{fontFamily: 'TTSupermolot-Regular'}}>Cases</h1>
+      <h1>Cases</h1>
+      <h1>Teste</h1>
       <Link to="/cases"></Link>
     </div>
   );
