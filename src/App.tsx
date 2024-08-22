463904c1ae62c2bff35a448ac96d90b3e15548ad
saulo.rodrigues <saulohrodrigues@gmail.com>
2024-08-22 08:41:20 -0300

    Refatorando o App, retornando ao status original

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import  DepoimentoComponent  from './components/Depoimento'
-import Rotas from './rotas/rotas'
 
 function App() {
-
+  const [count, setCount] = useState(0)
   return (
     <>
-      <Rotas/>
-      <DepoimentoComponent
-        autor='Saulo Henrique'
-        texto='Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit deleniti quibusdam provident quis doloribus rem corporis, molestiae nihil. Incidunt ut molestiae pariatur explicabo nihil quia voluptate sint officiis omnis assumenda.'
-        titulo='Depoimento'
-        img='/src/assets/fotoExemplo.jpg'
-      />
-
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
