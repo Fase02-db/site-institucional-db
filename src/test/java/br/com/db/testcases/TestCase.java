62e532a85cc41fb50d486b64127bfb54f1d6978a
loopfagundes <lof.fagundes@gmail.com>
2024-08-26 09:51:30 -0300

    fix: deletei o importado de logger

--- src/test/java/br/com/db/testcases/TestCase.java
+++ src/test/java/br/com/db/testcases/TestCase.java
@@ -1,7 +1,6 @@
 package br.com.db.testcases;
 
 import br.com.db.utils.BaseTest;
-import br.com.db.utils.LoggerFactory;
 import br.com.db.webdrivers.DriverManager;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
