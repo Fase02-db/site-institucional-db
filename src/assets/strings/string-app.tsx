48a8392aec80c7a836faa912da5a714d2449eb7b
Francis <francis.source@gmail.com>
2024-08-29 13:06:56 -0300

    criando a página de cases

--- src/assets/strings/string-app.tsx
+++ src/assets/strings/string-app.tsx
@@ -21,4 +21,15 @@ export const StringsApp = {
   tituloCardFinancas: 'Case Finanças',
   resumoDepoimento:
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis dolor nulla. Phasellus egestas purus ut quam malesuada, eu tristique tortor posuere. Phasellus massa arcu, rhoncus vitae maximus  vitae, lacinia a dui.',
+  tituloCase: 'Case Petlove',
+  resumoCasePrimeiroParagrafo:
+    'A Petlove, renomada empresa do setor pet, recentemente passou por uma transformação digital impressionante, graças ao projeto liderado pela empresa ABC. Este case fictício ilustra como a adoção de uma metodologia ágil, aliada ao uso de tecnologia de ponta e um compromisso com a qualidade, pode revolucionar uma organização. A parceria com a ABC permitiu à Petlove modernizar seus processos internos e aprimorar a experiência do cliente de maneira significativa.',
+  resumoCaseSegundoParagrafo:
+    'Utilizando a metodologia ágil, a ABC estruturou o projeto de forma a permitir entregas rápidas e contínuas. Isso facilitou a adaptação e o ajuste das soluções de acordo com as necessidades específicas da Petlove, sem comprometer os prazos. As equipes multifuncionais trabalharam de maneira colaborativa, promovendo uma comunicação eficiente e garantindo que cada fase do projeto estivesse alinhada com os objetivos estratégicos da Petlove. A flexibilidade do método ágil permitiu que a Petlove respondesse rapidamente às mudanças do mercado e às expectativas dos clientes.',
+  resumoCaseTerceiroParagrafo:
+    ' A transformação digital também envolveu a implementação de tecnologias de ponta, como inteligência artificial, análise de big data e plataformas de e-commerce avançadas. A ABC assegurou que todas as soluções tecnológicas fossem integradas de forma harmoniosa, otimizando a operação da Petlove e proporcionando uma experiência de compra personalizada e eficiente para os clientes. A dedicação à qualidade foi evidente em cada etapa do projeto, resultando em sistemas robustos e confiáveis que não apenas atenderam, mas superaram as expectativas da Petlove. Como resultado, a Petlove não só consolidou sua liderança no mercado, mas também estabeleceu novos padrões de excelência no setor pet.',
+  tituloCardDesign: 'Design Think para o setor pet',
+  tituloCardVeterinaria: 'Inteligência artificial na Veterinária',
+  tituloCardTecnoPet: 'Integrando tecnologia e bem-estar pet',
+  tituloServicosOferecidos: 'Serviços oferecidos',
 }
