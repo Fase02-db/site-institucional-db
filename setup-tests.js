cc8654be418ab3c636367f22e3da2d851a679ff2
saulo.rodrigues <saulohrodrigues@gmail.com>
2024-09-02 09:58:34 -0300

    Fix: retirando código duplicado do arquivo setup-tests.js

--- setup-tests.js
+++ setup-tests.js
@@ -17,15 +17,3 @@ beforeAll(() => {
     })),
   });
 });
-// jest.setup.js
-
-window.matchMedia = jest.fn().mockImplementation(() => ({
-  matches: false,
-  media: '',
-  onchange: null,
-  addListener: jest.fn(), // Obsoleto
-  removeListener: jest.fn(), // Obsoleto
-  addEventListener: jest.fn(),
-  removeEventListener: jest.fn(),
-  dispatchEvent: jest.fn(),
-}));
