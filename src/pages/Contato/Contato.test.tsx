08729a307ecf3dfc27e1bba18e940f5c71c4f599
saulo.rodrigues <saulohrodrigues@gmail.com>
2024-09-04 16:00:21 -0300

    Feat: Pagina de contatos finalizada

---
+++ src/pages/Contato/Contato.test.tsx
@@ -0,0 +1,10 @@
+import {fireEvent, render, screen } from '@testing-library/react'
+import Contato from './Contato';
+describe('Teste da pagina de contato', () => {
+  it('Deve renderizar os componentes do formulário', () => {
+    render(<Contato/>);
+    
+
+  });
+
+})
--- src/pages/Contato/Contato.tsx
+++ src/pages/Contato/Contato.tsx
@@ -1,13 +1,156 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import Formulario from '../../components/Formulario';
+
+const ContainerDiv = styled.div`
+    margin: 0px;
+    padding: 0px;
+    margin-bottom: 10px;
+    width: 100%;
+    height: auto; 
+    display: flex;
+    justify-content: space-around;
+    flex-direction: row;
+    flex-wrap: wrap;
+    border: solid 5px red;
+`
+const Imagem = styled.img`
+    width: 100%;
+    height: auto;  
+`
+const HeaderImgDiv = styled.div`
+    width: 100%;
+    height: 384px;  
+`
+const TituloDiv = styled.div`
+    margin: 55px 0px 55px 0px;
+    padding: 0; 
+    width: 71%;
+    height: 62px; 
+`
+const TituloH1 = styled.h1`
+    height: 45px;
+    font-family: 'TTSupermolot-Bold';
+    font-weight: 400;
+    font-size: 36px;
+    margin: 0;
+    padding: 0; 
+    line-height: 44.46px ;
+`
+const DadosEmpresa = styled.section`
+    margin: 0px;
+    padding: 0; 
+    width: 71%;
+    height: 502px; 
+    align-items: center;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;  
+`
+const FotoEmpresaDiv = styled.div`
+    width: 45%;
+    height: 100%; 
+`
+const InformacoesEmpresaDiv = styled.div`
+    width: 49%;
+    height: 100%; 
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    justify-content: space-between;   
+  `
+const EnderecoFilialDiv = styled.div`
+    width: 100%;
+    height: 45%; 
+    gap: 19px;
+    text-align: left;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    justify-content: space-between; 
+    
+`
+const H5 = styled.h5`
+    height: 44px;
+    font-family: 'TTSupermolot-Regular';
+    font-weight: 400;
+    font-size: 18px;
+    margin: 0px;
+    padding: 0px; 
+    line-height: 22.14px;
+  
+`
+const EntreEmContatoDiv = styled.div`
+    clear: both;
+    margin: 55px 0px 55px 0px;
+    padding: 0; 
+    width: 71%;
+    height: 600px; 
+`
+const TelefoneDiv = styled.div`
+    width: 225px;
+    height: 44px; 
+    padding:10px 10px 10px 0px;
+    gap:10px;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    justify-content: space-between;
+`
 
 const Contato: React.FC = () => {
   return (
-    <div>
-      <h1>Contato</h1>
-      <Link to="/contato"></Link>
-    </div>
+    <ContainerDiv id='principal'>
+      <HeaderImgDiv>
+        <Imagem src='./src/assets/topContato.png'/>
+      </HeaderImgDiv>
+      <TituloDiv>
+        <TituloH1>Nossos endereços</TituloH1>
+      </TituloDiv>
+      <DadosEmpresa>
+        <FotoEmpresaDiv>
+          <Imagem src='./src/assets/empresaRS.png'/>
+        </FotoEmpresaDiv>
+        <InformacoesEmpresaDiv>
+          <EnderecoFilialDiv>
+              <TituloH1>Rio Grande do Sul</TituloH1>
+              <H5>
+                  Av. Ipiranga, 6681 - TECNOPUC - Prédio 99A - 5º Andar |CEP 90619-900 | 
+                  Bairro Partenon | Porto Alegre | RS | Brasil |
+              </H5>
+              <TelefoneDiv>
+                  <div>
+                      <Imagem src='./src/assets/iconePhone.png'/>
+                  </div>
+                  <H5>
+                    Telefone: +55 (51) 3322-6828
+                  </H5>
+              </TelefoneDiv>
+          </EnderecoFilialDiv>
+
+          <EnderecoFilialDiv>
+          <TituloH1>São Paulo</TituloH1>
+              <H5>
+              Av. das Nações Unidas 12901, Torre Norte, 12. Andar |WeWork/CENU | 04578-910 
+              | Bairro Brooklin Paulista | São Paulo | SP | Brasil |
+              </H5>
+              <TelefoneDiv>
+                  <div>
+                      <Imagem src='./src/assets/iconePhone.png'/>
+                  </div>
+                  <H5>
+                    Telefone: +55 (11) 3064-0810
+                  </H5>
+              </TelefoneDiv>  
+          </EnderecoFilialDiv>
+
+        </InformacoesEmpresaDiv>
+      </DadosEmpresa>
+      <EntreEmContatoDiv>
+        <Formulario/>
+      </EntreEmContatoDiv>
+    </ContainerDiv>
   );
 };
-
 export default Contato;
